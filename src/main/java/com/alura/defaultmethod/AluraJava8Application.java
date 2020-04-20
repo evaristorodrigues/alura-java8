17be9eb872b36633d33bf9947305f9640790b2dc
Evaristo S. Rodrigues <evaristosrodrigues@pop-os.localdomain>
2020-04-20 19:29:42 -0300

    -criando o projeto

---
+++ src/main/java/com/alura/defaultmethod/AluraJava8Application.java
@@ -0,0 +1,13 @@
+package com.alura.defaultmethod;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AluraJava8Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AluraJava8Application.class, args);
+	}
+
+}
